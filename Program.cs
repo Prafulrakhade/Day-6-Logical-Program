fdfe5b026f887223268bd9311000cc4464d4a380
Praful <prafulrakhade02@gmail.com>
2022-03-13 17:46:28 +0530

    [praful] Added: Reverse Number Program File

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ReverseNumber
+{
+    internal class Program
+    {
+
+        public void ReverseNumber()
+        {
+            int num;
+            int rem=0;
+            int reverse = 0;
+            Console.Write("Enter the Number : ");
+            num = int.Parse(Console.ReadLine());
+            while(num!=0)
+            {
+                rem = num % 10;
+                reverse = reverse * 10 + rem;
+                num /= 10;
+            }
+            Console.WriteLine("Reversed Number : " + reverse);
+        }
+        static void Main(string[] args)
+        {
+            Program check = new Program();
+            check.ReverseNumber();
+        }
+    }
+}
