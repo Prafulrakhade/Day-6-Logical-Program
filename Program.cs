3b13ed9dc3d274155593571821975fe75832e3bf
Praful <prafulrakhade02@gmail.com>
2022-03-13 17:34:20 +0530

    [praful] Added: Perfect Number program File

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace PerfectNumber
+{
+    internal class Program
+    {
+        public void perfectNumber()
+        {
+            int num;
+            int sum = 0;
+            Console.Write("Enter the number = ");
+            num = int.Parse(Console.ReadLine());
+            for(int i=1; i<num; i++)
+            {
+                if(num % i == 0)
+                {
+                    sum = sum + i;
+                }
+            }
+            if(sum==num)
+            {
+                Console.WriteLine(num+" is a Perfect Number");
+            }
+            else
+            {
+                Console.WriteLine(num+" is not a Perfect Number");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Program check = new Program();
+            check.perfectNumber();
+        }
+    }
+}
