c1bdb86a27e5f96e1e17a0ecafa3f622096aab69
Praful <prafulrakhade02@gmail.com>
2022-03-13 17:43:36 +0530

    [praful] Added: Prime Number Program file

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PrimeNumber
+{
+    internal class Program
+    {
+        public void PrimeNumber()
+        {
+            int num;
+            Console.Write("enter the number = ");
+            num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 2; i <= num / 2; i++)
+            {
+                if (num % i == 0)
+                {
+                    Console.WriteLine(" is not Prime Number ");
+                }
+                else
+                {
+                    Console.WriteLine(" is a Prime Number ");
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Program check = new Program();
+            check.PrimeNumber();
+        }
+    }
+}
